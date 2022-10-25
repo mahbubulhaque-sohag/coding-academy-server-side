fa7fb518791324a41039d89e233c5fc18d7500e5
unknown <mahbubulsohag810@gmail.com>
2022-10-25 16:37:52 +0600

    added course data in couses.json

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const Port = process.env.Port || 5000
+
+const couseData  = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('coding academy api running')
 })
 
-app.listen(port, () =>{
-    console.log('coding-academy api running on', port)
+app.get('/courses', (req,res)=> {
+    res.send(couseData)
+})
+
+app.listen(Port, () =>{
+    console.log('coding-academy api running on', Port)
 })
